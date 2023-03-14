2b6f61f9eacc6fa119000538c72f9caf35da2be0
Yoshikazuuu <febrianojerry@gmail.com>
2023-03-14 15:43:34 +0700

    fix: user avatar bugs when showing gif

--- src/commands/user.js
+++ src/commands/user.js
@@ -1,10 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const { ERROR_LOG } = require("../utils/log_template");
-const { REST } = require("@discordjs/rest");
-const { Routes } = require("discord-api-types/v10");
-const { token } = require("../../config.json");
-
-const rest = new REST({ version: "10" }).setToken(token);
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -27,20 +22,22 @@ module.exports = {
     try {
       if (interaction.options.getSubcommand() === "avatar") {
         let id = interaction.options.getUser("user");
+        let avatar;
 
-        id = id ? id.id : interaction.user.id;
-
-        const data = await rest.get(Routes.user(id));
+        if (id) {
+          avatar = id.displayAvatarURL();
+        } else {
+          id = interaction.user;
+          avatar = id.displayAvatarURL();
+        }
 
         const embed = new EmbedBuilder()
           .setColor("#F6C1CC")
           .setAuthor({
             name: "User Info",
           })
-          .setTitle(`${data.username}#${data.discriminator}'s avatar.`)
-          .setImage(
-            `https://cdn.discordapp.com/avatars/${data.id}/${data.avatar}.webp?size=512`
-          )
+          .setTitle(`${id.username}#${id.discriminator}'s avatar.`)
+          .setImage(`${avatar}?size=512`)
           .setTimestamp()
           .setFooter({
             text: `Requested by ${interaction.user.username}#${interaction.user.discriminator}`,
